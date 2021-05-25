b5c07cea63397c8deb16e8d975e40c5a713a2b55
bramag94 <bramag94@hotmail.com>
2021-05-25 03:59:56 +0000

    todas las funciones/postman trabajando

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Todos
 #from models import Person
 
 app = Flask(__name__)
@@ -47,6 +47,47 @@ def main_todos():
     }
 
     return jsonify(response_body), 200
+
+#Get all todos#
+
+@app.route('/mytodos', methods=['GET'])
+def get_todos():
+    mis_todos = Todos.query.all()
+    all_todos = list(map(lambda x: x.serialize(), mis_todos))
+    return jsonify(all_todos), 200
+
+#Get todo by id#
+@app.route('/mytodos/<id>', methods=['GET'])
+def one_todo(id):
+    one_todo = Todos.query.filter_by(id=id).first()
+    if not one_todo:
+        return APIException("No se encontró la tarea",status_code=404)
+    request_body = one_todo.serialize()
+    return jsonify(request_body),200
+
+
+#Post todo
+
+@app.route('/mytodos', methods=['POST'])
+def create_todo():
+    data = request.get_json()
+    mytodo = Todos(label=data["label"],done=data["done"]) #is_active=data["is_active"]
+    db.session.add(mytodo)
+    db.session.commit()
+    return jsonify ("Message : Se adiciono una nueva tarea!"), 200
+
+
+#Eliminar todo
+
+@app.route('/mytodos/<id>', methods=['DELETE'])
+def delete_mytodo(id):
+    my_todo =  Todos.query.get(id)
+    if not one_todo:
+        return APIException("No se encontró la tarea",status_code=404)
+    db.session.delete(my_todo)
+    db.session.commit() 
+    return jsonify("Succesfully eliminated!"), 200
+
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
